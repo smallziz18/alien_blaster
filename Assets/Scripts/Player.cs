0aaf1ad52c9c322f3832d83d225ac1320d8e0625
smallziz <abdaziz.diouf@astarex-ps.com>
2026-02-15 10:16:44 +0000

    update player movement and change input handler setting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Debug.Log("Update at" + Time.time);
+    {   rb = GetComponent<Rigidbody2D>();
+        var horizontal = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal, rb.linearVelocity.y);
     }
 }
